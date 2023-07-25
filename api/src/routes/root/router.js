823032da25fef521298b5aa69d0fb3f6212b1127
Morgan Bastian <morganbastian07@gmail.com>
2023-07-25 16:32:27 -0400

    init project

---
+++ api/src/routes/root/router.js
@@ -0,0 +1,12 @@
+const { Router } = require('express')
+const { index } = require('./controller')
+
+// create a new Router instance
+const router = new Router()
+
+// define routes
+
+router.get('/', index)
+
+// exporting router
+module.exports = router
