a8fd6e0fefc7a61fc31b235454f37728b769a179
Morgan Bastian <morganbastian07@gmail.com>
2023-07-28 11:26:00 -0400

    cleaned up navbar

--- ui/src/layouts/Navbar.js
+++ ui/src/layouts/Navbar.js
@@ -1,82 +1,93 @@
-import { useEffect, useState } from "react"
-import { Link } from 'react-router-dom';
-import {Paper, AppBar, Box, Toolbar, IconButton, Typography, Menu, Container, Avatar, Button, Tooltip, MenuItem } from "@mui/material";
-import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
-import NotificationsIcon from "@mui/icons-material/Notifications";
-import { getToken, isUserLoggedIn, setToken, clearToken } from "../utility/utils";
-import { getMe } from "../utility/api";
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import {
+	Paper,
+	AppBar,
+	Box,
+	Toolbar,
+	IconButton,
+	Typography,
+	Menu,
+	Container,
+	Avatar,
+	Button,
+	Tooltip,
+	MenuItem,
+} from '@mui/material'
+
+import {
+	isUserLoggedIn,
+	clearToken,
+} from '../utility/utils'
+import { getMe } from '../utility/api'
 
 //settings for the profile dropdown
 function Navbar() {
+	const [anchorElNav, setAnchorElNav] = useState(null)
+	const [anchorElUser, setAnchorElUser] = useState(null)
+	const [myData, setMyData] = useState({})
+	
 
-	const [anchorElNav, setAnchorElNav] = useState(null);
-	const [anchorElUser, setAnchorElUser] = useState(null);
-  const [myData, setMyData] = useState({});
-  const settings = ["Profile", "Account", "Logout"];
+	useEffect(() => {
+		if (isUserLoggedIn()) {
+			const getMyData = async () => {
+				const me = await getMe()
+				setMyData(me)
+			}
+			getMyData()
+		}
+	}, [])
 
-  useEffect(() => {
-    if (isUserLoggedIn()) {
-      const getMyData = async () => {
-        const me = await getMe();
-        setMyData(me);
-      };
-      getMyData();
-    }
-  }, []);
-
-  const isUserAdmin = () => {
-    if (myData.role === "admin") {
-      return true
-    }
+	const isUserAdmin = () => {
+		if (myData.role === 'admin') {
+			return true
+		}
 		return false
-  };
+	}
 
-  function Logout() {
-    clearToken();
-    window.location.reload(false);
-  }
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
+	function Logout() {
+		clearToken()
+		window.location.reload(false)
+	}
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+	const handleOpenUserMenu = (event) => {
+		setAnchorElUser(event.currentTarget)
+	}
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+	const handleCloseNavMenu = () => {
+		setAnchorElNav(null)
+	}
 
-  if (!myData) {
-    return <div>loading...</div>;
-  }
+	const handleCloseUserMenu = () => {
+		setAnchorElUser(null)
+	}
+
+	if (!myData) {
+		return <div>loading...</div>
+	}
 
 	return (
 		<Paper>
-			<AppBar position="static">
-				<Container maxWidth="xl">
+			<AppBar position='static'>
+				<Container maxWidth='xl'>
 					<Toolbar disableGutters>
 						{/* linked the iWitness text to the homepage */}
-						<Link
-							to="/"
-							style={{ textDecoration: "none", color: "white" }}>
+						<Link to='/' style={{ textDecoration: 'none', color: 'white' }}>
 							<Typography
-								variant="h6"
+								variant='h6'
 								noWrap
-								component="a"
-								href="/"
+								component='a'
+								href='/'
 								sx={{
 									mr: 2,
-									display: { xs: "none", md: "flex" },
-									fontFamily: "monospace",
+									display: { xs: 'none', md: 'flex' },
+									fontFamily: 'monospace',
 									fontWeight: 700,
-									letterSpacing: ".3rem",
-									color: "inherit",
-									textDecoration: "none",
-								}}>
+									letterSpacing: '.3rem',
+									color: 'inherit',
+									textDecoration: 'none',
+								}}
+							>
 								Shem Creek Charter Co
 							</Typography>
 						</Link>
@@ -84,103 +95,116 @@ function Navbar() {
 						<Box
 							sx={{
 								flexGrow: 1,
-								display: { xs: "flex", md: "none" },
-								alignItems: "center",
-							}}>
+								display: { xs: 'flex', md: 'none' },
+								alignItems: 'center',
+							}}
+						>
 							<Menu
-								id="menu-appbar"
+								id='menu-appbar'
 								anchorEl={anchorElNav}
 								anchorOrigin={{
-									vertical: "bottom",
-									horizontal: "left",
+									vertical: 'bottom',
+									horizontal: 'left',
 								}}
 								keepMounted
 								transformOrigin={{
-									vertical: "top",
-									horizontal: "left",
+									vertical: 'top',
+									horizontal: 'left',
 								}}
 								open={Boolean(anchorElNav)}
 								onClose={handleCloseNavMenu}
 								sx={{
-									display: { xs: "block", md: "none" },
-						}}></Menu>
+									display: { xs: 'block', md: 'none' },
+								}}
+							></Menu>
 						</Box>
 						<Box
 							sx={{
 								flexGrow: 1,
-								display: { xs: "none", md: "flex" },
-								flexDirection: "flex-end",
-								alignItems: "center",
-								justifyContent: "flex-end",
-								textDecoration: "none",
-								color: "white",
-							}}>
-							<Link
-								to="/booking"
-								style={{ textDecoration: "none" }}>
+								display: { xs: 'none', md: 'flex' },
+								flexDirection: 'flex-end',
+								alignItems: 'center',
+								justifyContent: 'flex-end',
+								textDecoration: 'none',
+								color: 'white',
+							}}
+						>
+							<Link to='/booking' style={{ textDecoration: 'none' }}>
 								<Button
 									sx={{
-										display: { xs: "none", md: "flex" },
+										display: { xs: 'none', md: 'flex' },
 										mr: 1,
-										textDecoration: "none",
-										color: "white",
-									}}>
+										textDecoration: 'none',
+										color: 'white',
+									}}
+								>
 									Book Now
 								</Button>
 							</Link>
 						</Box>
-            <Box sx={{ flexGrow: 0 }}>
+						<Box sx={{ flexGrow: 0 }}>
 							<Tooltip>
 								{/* profile avatar on far right side */}
 								{isUserLoggedIn() ? (
-									<IconButton
-										onClick={handleOpenUserMenu}
-										sx={{ p: 0 }}>
-										<Avatar alt="Profile" />
+									<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+										<Avatar alt='Profile' />
 									</IconButton>
 								) : (
-									<Link to="/login">
+									<Link to='/login'>
 										<Button
 											sx={{
 												// display: { xs: 'none', md: 'flex' },
 												mr: 1,
-												textDecoration: "none",
-												color: "white",
-												flex: "row-reverse",
-											}}>
+												textDecoration: 'none',
+												color: 'white',
+												flex: 'row-reverse',
+											}}
+										>
 											Login
 										</Button>
 									</Link>
 								)}
 							</Tooltip>
 							<Menu
-								sx={{ mt: "45px" }}
-								id="menu-appbar"
+								sx={{ mt: '45px' }}
+								id='menu-appbar'
 								anchorEl={anchorElUser}
 								anchorOrigin={{
-									vertical: "top",
-									horizontal: "right",
+									vertical: 'top',
+									horizontal: 'right',
 								}}
 								keepMounted
 								transformOrigin={{
-									vertical: "top",
-									horizontal: "right",
+									vertical: 'top',
+									horizontal: 'right',
 								}}
 								open={Boolean(anchorElUser)}
 								onClose={handleCloseUserMenu}
 							>
-								<MenuItem><Link to='/userpanel' style={{ textDecoration: 'none' }}>My Bookings</Link></MenuItem>
-                {!isUserAdmin ? null : 
-                  <MenuItem><Link to='/adminpanel' style={{ textDecoration: 'none' }}>Admin Panel</Link></MenuItem> 
-                }
-								<MenuItem onClick={() => Logout()}><Link to='/' style={{ textDecoration: 'none' }}>Logout</Link></MenuItem>
+								<MenuItem>
+									<Link to='/userpanel' style={{ textDecoration: 'none' }}>
+										My Bookings
+									</Link>
+								</MenuItem>
+								{!isUserAdmin ? null : (
+									<MenuItem>
+										<Link to='/adminpanel' style={{ textDecoration: 'none' }}>
+											Admin Panel
+										</Link>
+									</MenuItem>
+								)}
+								<MenuItem onClick={() => Logout()}>
+									<Link to='/' style={{ textDecoration: 'none' }}>
+										Logout
+									</Link>
+								</MenuItem>
 							</Menu>
 						</Box>
 					</Toolbar>
 				</Container>
 			</AppBar>
 		</Paper>
-	);
+	)
 }
 
-export default Navbar;
+export default Navbar
