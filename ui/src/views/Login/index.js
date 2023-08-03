309da2c6b89e6c1da55eb5186bd545070e8a5f05
Morgan Bastian <morganbastian07@gmail.com>
2023-08-03 01:40:55 -0400

    added mui to views

--- ui/src/views/Login/index.js
+++ ui/src/views/Login/index.js
@@ -1,4 +1,12 @@
-import { Grid, Typography, Button } from '@mui/material'
+import Avatar from '@mui/material/Avatar'
+import Button from '@mui/material/Button'
+import CssBaseline from '@mui/material/CssBaseline'
+import TextField from '@mui/material/TextField'
+import Grid from '@mui/material/Grid'
+import Box from '@mui/material/Box'
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
+import Typography from '@mui/material/Typography'
+import Container from '@mui/material/Container'
 import { login } from '../../utility/api'
 import { setToken } from '../../utility/utils'
 import { useState } from 'react'
@@ -44,48 +52,69 @@ function Login(props) {
 	}
 
 	return (
-		<div>
-			<Grid
-				container
-				spacing={0}
-				direction='column'
-				alignItems='center'
-				justifyContent='center'
-				padding={'20px'}
-				sx={{ border: 'solid black 2px' }}
+		<Container component='main' maxWidth='xs'>
+			<CssBaseline />
+			<Box
+				sx={{
+					marginTop: 8,
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'center',
+					paddingBottom: 10,
+				}}
 			>
-				<form onSubmit={handleSubmit}>
-					<Grid item xs={8}>
-						<label>
-							Username:
-							<input
-								type='text'
-								name='username'
-								value={userData.username}
-								onChange={handleChange}
-							/>
-						</label>
+				<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+					<LockOutlinedIcon />
+				</Avatar>
+				<Typography component='h1' variant='h5'>
+					Login
+				</Typography>
+				<Box component='form' onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+					<TextField
+						margin='normal'
+						required
+						fullWidth
+						id='username'
+						label='Username'
+						name='username'
+						value={userData.username}
+						onChange={handleChange}
+						autoFocus
+					/>
+					<TextField
+						margin='normal'
+						required
+						fullWidth
+						name='password'
+						label='Password'
+						type='password'
+						id='password'
+						onChange={handleChange}
+						value={userData.password}
+					/>
+
+					<Button
+						onSubmit={handleSubmit}
+						type='submit'
+						fullWidth
+						variant='contained'
+						sx={{ mt: 3, mb: 2 }}
+					>
+						Login
+					</Button>
+					<Grid container>
+						<Grid item>
+							<Link to='/register' variant='body2'>
+								{"Don't have an account? Sign Up"}
+							</Link>
+						</Grid>
 					</Grid>
-					<Grid item xs={8}>
-						<label>
-							Password:
-							<input
-								type='password'
-								name='password'
-								value={userData.password}
-								onChange={handleChange}
-							/>
-						</label>
-					</Grid>
-					<input type='submit' value='Submit' />
-					<Link to='/register'>
-						<Button>Register</Button>
-					</Link>
-				</form>
-				<div>{checkLoginError()}</div>
-			</Grid>
-		</div>
+				</Box>
+			</Box>
+			<div>{checkLoginError()}</div>
+		</Container>
 	)
 }
+
 
 export default Login
