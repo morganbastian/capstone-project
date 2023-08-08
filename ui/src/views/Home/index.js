c4d9019b60b833a3d5975f26a4f820c8ddbfd99e
Morgan Bastian <morganbastian07@gmail.com>
2023-08-08 16:05:25 -0400

    changed lorem ipso

--- ui/src/views/Home/index.js
+++ ui/src/views/Home/index.js
@@ -13,11 +13,19 @@ function Home(props) {
 
 	//** Return JSX
 	return (
-		<Paper elevation={0} >
-		<div style={{ background: '#f3e9ca'}}>
-      <img src="https://i.ibb.co/dBBdHMb/SCC14.jpg" alt="Harbor Cruise" style={{ width: '50%', height: 300 }}/>
-			<img src="https://i.ibb.co/ph4fykd/IMG-0807.jpg" alt="Pleasant Lady"  style={{ width: '50%', height: 300 }}/>
-    </div>
+		<Paper elevation={0}>
+			<div style={{ background: '#f3e9ca' }}>
+				<img
+					src='https://i.ibb.co/dBBdHMb/SCC14.jpg'
+					alt='Harbor Cruise'
+					style={{ width: '50%', height: 300 }}
+				/>
+				<img
+					src='https://i.ibb.co/ph4fykd/IMG-0807.jpg'
+					alt='Pleasant Lady'
+					style={{ width: '50%', height: 300 }}
+				/>
+			</div>
 			<Box
 				sx={{
 					paddingLeft: '1rem',
@@ -25,11 +33,12 @@ function Home(props) {
 					width: '100%',
 					height: 'auto',
 					paddingTop: '1rem',
-					background: '#f3e9ca'
-					
+					background: '#f3e9ca',
 				}}
 			>
-				<Typography variant='h5'><strong>Harbor Cruise</strong></Typography>
+				<Typography variant='h5'>
+					<strong>Harbor Cruise</strong>
+				</Typography>
 				<Typography variant='h6'>
 					Welcome aboard the Pleasant Lady - a luxurious sixty-foot 1979
 					converted fishing boat, renovated into a one-of-a-kind harbor cruiser.
@@ -51,14 +60,23 @@ function Home(props) {
 					to='/bookings/new'
 					style={{ textDecoration: 'none', color: 'white' }}
 				>
-					<Button variant='contained' sx={{marginTop: 3}}>Book Now</Button>
+					<Button variant='contained' sx={{ marginTop: 3 }}>
+						Book Now
+					</Button>
 				</Link>
 			</Box>
-			<div style={{paddingTop: 25, background: '#f3e9ca' }}>
-      <img src="https://stpetepier.org/wp-content/uploads/2022/06/ctb-0775-june-2022-blog-tour-boat-main-1200x768-1.jpg" alt="Eco-Tour" style={{ width: '50%', height: 300 }}/>
-			<img src="https://lowcountrywatersports.com/wp-content/uploads/2023/05/IMG_9794.jpg" alt="Dolphins" style={{ width: '50%', height: 300 }}/>
-
-    </div>
+			<div style={{ paddingTop: 25, background: '#f3e9ca' }}>
+				<img
+					src='https://stpetepier.org/wp-content/uploads/2022/06/ctb-0775-june-2022-blog-tour-boat-main-1200x768-1.jpg'
+					alt='Eco-Tour'
+					style={{ width: '50%', height: 300 }}
+				/>
+				<img
+					src='https://lowcountrywatersports.com/wp-content/uploads/2023/05/IMG_9794.jpg'
+					alt='Dolphins'
+					style={{ width: '50%', height: 300 }}
+				/>
+			</div>
 			<Box
 				sx={{
 					width: '100%',
@@ -67,28 +85,29 @@ function Home(props) {
 					paddingRight: '1rem',
 					paddingTop: '1rem',
 					paddingBottom: '25px',
-					background: '#f3e9ca'
+					background: '#f3e9ca',
 				}}
 			>
-				<Typography variant='h5'><strong>Eco-Tour</strong></Typography>
+				<Typography variant='h5'>
+					<strong>Eco-Tour</strong>
+				</Typography>
 				<Typography variant='h6'>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin turpis
-					mauris, fringilla quis malesuada at, vestibulum a tellus. Donec ornare
-					elementum velit, non ultricies nunc lacinia et. Pellentesque ac
-					aliquet arcu. Duis consequat eros non dolor vehicula semper. Etiam
-					cursus urna sit amet fringilla interdum. Duis porttitor pharetra
-					massa, quis varius elit dapibus vitae. Donec id enim rutrum sem
-					molestie fermentum eu feugiat lectus. Nam facilisis molestie gravida.
-					Mauris quis nibh sem. In porta eget lorem eu facilisis. Praesent
-					pellentesque ex odio, at lobortis urna sollicitudin vel. Sed et lectus
-					vulputate, suscipit velit ut, rutrum nisi. Cras pulvinar sapien id
-					pretium viverra.
+					Guided by knowledgeable naturalists, tourists are educated about the
+					diverse flora and fauna while they soak in the natural beauty of
+					Charleston's waters. Through responsible practices and a commitment to
+					conservation, we provide an unforgettable
+					experience that fosters appreciation for both the environment and
+					local culture. Whether it's observing playful dolphins or uncovering
+					hidden marshlands, this unique tour promotes a harmonious connection
+					between nature and tourism.
 				</Typography>
 				<Link
 					to='/bookings/new'
 					style={{ textDecoration: 'none', color: 'white' }}
 				>
-					<Button variant='contained' sx={{marginTop: 3}}>Book Now</Button>
+					<Button variant='contained' sx={{ marginTop: 3 }}>
+						Book Now
+					</Button>
 				</Link>
 			</Box>
 			<Footer />
